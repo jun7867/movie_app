24fd8d113ccd411f0dadaa10cc95261cddd88156
남쭌 <jjune095@naver.com>
2020-07-13 00:00:47 +0900

    jsx and component practice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Potato from "./potato" // import 해줘야함.
 
+// component는 html을 반환해주는 함수이다.
+// <App/>  이를 jsx로 말한다.(javascript+ html) react 에서만 쓰임.
+// 한번에 한개의 component만 render할 수 있다. 따라서 다른 component에 집어 넣어서 사용해야함.(ex) potato.js
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/potato.js
@@ -0,0 +1,7 @@
+import React from 'react'; // 이걸 해줘야지만 component 사용 가능.
+
+function Potato(){
+    return <h3>I love potato!</h3>;
+}
+
+export default Potato;
