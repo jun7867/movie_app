d765e59d38c8a18c9fd9d7bf1bb31c47eb3bb6ca
남쭌 <jjune095@naver.com>
2020-07-23 11:14:44 +0900

    Practice State

--- src/App.js
+++ src/App.js
@@ -1,42 +1,27 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import PropTypes from "prop-types";
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "https://www.bgw.kr/wp-content/uploads/2019/12/%EC%88%98%EC%9E%85%EA%B9%80%EC%B9%98.png",
-  },
-  {
-    id: 2,
-    name: "coke",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/51jKm1MY43L._SX569_.jpg",
-  },
-];
-
-// function renderFood(food) {
-//   return <Food name={food.name} picture={food.image} />;
-// }
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((food) => (
-        <Food key={food.id} name={food.name} picture={food.image} />
-      ))}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count} </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 //import Potato from "./potato" // import 해줘야함.
 
-// component는 html을 반환해주는 함수이다.
+// component는 html을 반환해주는 함수이다.(function App / return)
 // <App/>  이를 jsx로 말한다.(javascript+ html) react 에서만 쓰임.
+
 // 한번에 한개의 component만 render할 수 있다. 따라서 다른 component에 집어 넣어서 사용해야함.(ex) potato.js
 ReactDOM.render(
   <React.StrictMode>
