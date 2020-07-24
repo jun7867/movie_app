24a3ecbe769ed05c535c13b1d01eac89523b06fa
남쭌 <jjune095@naver.com>
2020-07-24 10:49:47 +0900

    CSS version1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
 import "./App.css";
+
 class App extends React.Component {
   state = {
     isLoading: true,
@@ -33,17 +34,19 @@ class App extends React.Component {
             <span className="loader__text">Loading..</span>
           </div>
         ) : (
-          movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-              genres={movie.genres}
-            />
-          ))
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
         )}
       </section>
     );
